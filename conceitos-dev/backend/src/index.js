87f5389f3ff71eafe0edd62e1ee03db821f0c04e
roddcouto <roddcouto@gmail.com>
2021-09-02 07:36:36 -0300

    novos plugins babel

--- conceitos-dev/backend/src/index.js
+++ conceitos-dev/backend/src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { v4: uuid, validate: isUuid, v4, validate } = require('uuid');
-
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const projects = [];
--- conceitos-dev/frontend/src/App.js
+++ conceitos-dev/frontend/src/App.js
@@ -1,26 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css'
-import backgroundImage from './assets/notebookwebp.jpeg'
 
 import Header from './components/Header';
 
 function App() {
-    const [projects, setProjects] = useState(['Desenvolvimento de App', 'Front-End Web']);
+    const [projects, setProjects] = useState([]);
 
-    function handleAddProject() {
-        //projects.push(`Novo projeto ${Date.now()}`);
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        })
+    }, []);
+
+    async function handleAddProject() {
+        /*//projects.push(`Novo projeto ${Date.now()}`);
         setProjects([...projects, `Novo projeto ${Date.now()}`]);
-        console.log(projects);
+        console.log(projects);*/
+
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Rodrigo Couto"
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
     };
 
     return (
         <>
            <Header title="Projects" />
 
-           <img width={980}src={backgroundImage} />
+           
            <ul>
-               {projects.map(project => <li key={project}>{project}</li>)}
+               {projects.map(project => <li key={project.id}>{project.title}</li>)}
            </ul>
 
            <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
