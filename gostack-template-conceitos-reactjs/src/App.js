801dbd2e2ac54b00a05c268d64857196da8776e2
roddcouto <roddcouto@gmail.com>
2021-09-22 09:34:38 -0300

    Adiciona um repositório na lista.

--- gostack-template-conceitos-reactjs/src/App.js
+++ gostack-template-conceitos-reactjs/src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Umbriel',
+      url: 'https://github.com/rocketseat/umbriel',
+      techs: ['Node.js', 'ReactJS']
+    });
+
+    setRepositories([ ...repositories, response.data ]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +30,15 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(respository => (
+          <li key={respository.id}>
+          {respository.title}
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
